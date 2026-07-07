de734096843e102b28955abaef3ecc70a9579525
Abhishek Kumar <abhishekkrmay30@gmail.com>
2026-07-07 21:43:16 +0530

    First Code

---
+++ Basics/BasicOutput.java
@@ -0,0 +1,13 @@
+public class BasicOutput {
+    public static void main(String[] args) {
+
+        String name = "Java";
+
+        System.out.print("Hello ");
+
+        System.out.println("World!"); 
+
+        System.out.println("Welcome to " + name + "!\nThis is on a new line.");
+        
+    }
+}
