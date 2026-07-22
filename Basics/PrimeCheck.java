7e1275ee339c396cd126d2696abc7d959cf6b0eb
Abhishek Kumar <abhishekkrmay30@gmail.com>
2026-07-22 21:40:20 +0530

    prime-check-added

---
+++ Basics/PrimeCheck.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        sc.close();
+        if (n <= 1) {
+            System.out.println("Neither Prime nor Composite");
+            return;
+        }
+
+        boolean isPrime = true;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(n + " is a Prime Number.");
+        } else {
+            System.out.println(n + " is a Composite Number.");
+        }
+    }
+}
