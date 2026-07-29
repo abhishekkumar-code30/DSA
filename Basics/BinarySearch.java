416bf501ad64525c504e8be533acd624b2a98cf7
Abhishek Kumar <abhishekkrmay30@gmail.com>
2026-07-29 15:03:38 +0530

    added-binarySearch

---
+++ Basics/BinarySearch.java
@@ -0,0 +1,34 @@
+public class BinarySearch {
+    public static int binarySearch(int[] arr, int target){
+        int left = 0;
+        int right = arr.length - 1;
+        int mid;
+        
+        while(left<=right){
+            mid = left + (right - left) / 2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if (arr[mid] < target) {
+                left = mid + 1;
+            }
+            else{
+                right = mid -1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,8,10,12,14,16,18,20};
+        int target = 2;
+
+        int result = binarySearch(arr, target);
+
+        if(result != -1){
+            System.out.println("Element found at : "+result);
+        }
+        else{
+            System.out.println("Element not found");
+        }
+    }
+}
